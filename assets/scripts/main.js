6cad5974ff585b0ddcc16ad9eb94b6b09a63d549
Galy4a <galy4a@mail.ru>
2015-09-24 23:56:18 +0600

    3
    3

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,11 +3,11 @@ jQuery(document).ready(function() {
 	$(".frame").mousemove(function(e){
     var X = e.pageX; // положения по оси X
     var Y = e.pageY; // положения по оси Y
-    $(".background").css({"top": 0-Y/50+"px", "left": 0-X/50+"px"});
-    $(".mount").css({"top": 0-Y/40+"px", "left": 0-X/40+"px"});
-    $(".mounts").css({"top": 0-Y/30+"px", "left": 0-X/30+"px"});
-    $(".forest-far").css({"top": 0+Y/30+"px", "left": 0+X/30+"px"});
-    $(".forest").css({"top": 0+Y/15+"px", "left": 0+X/15+"px"});
+    $(".background").css({"top": 30-Y/50+"px", "left": 30-X/50+"px"});
+    $(".mount").css({"top": 30-Y/40+"px", "left": 30-X/40+"px"});
+    $(".mounts").css({"top": 30-Y/30+"px", "left": 30-X/30+"px"});
+    $(".forest-far").css({"top": 30+Y/30+"px", "left": 30+X/30+"px"});
+    $(".forest").css({"top": 30+Y/15+"px", "left": 30+X/15+"px"});
 	}); 
 
 });
